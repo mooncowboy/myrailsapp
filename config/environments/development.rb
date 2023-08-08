73448d9125c9afd6a9efd214aeac9b64d89a4ab6
Ricardo Fiel <rifiel@microsoft.com>
2023-08-08 13:48:20 +0100

    config.hosts.clear for dev

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,4 +67,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # Enable codespaces URLs
+  config.hosts.clear
 end
