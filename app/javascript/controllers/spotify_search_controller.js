6c59aa941c2dfe1c1cbdc94222ca67ee5b52059e
Ricardo Fiel <rifiel@microsoft.com>
2023-09-15 11:46:31 +0000

    Skipping stimulus

--- app/javascript/controllers/spotify_search_controller.js
+++ app/javascript/controllers/spotify_search_controller.js
@@ -16,6 +16,30 @@ export default class extends Controller {
       .then(data => {
         console.log(data);
         // Do something with the data (like update the DOM)
+        // This returns a list of artists from Spotify. Show the first one in an alert.
+        // if (data.artists.length > 0) {
+        //   alert(data.artists[0].name);
+        // }
+
+    // Get the artists from the data object
+    const artists = data.artists;
+
+    // Create a list element
+    const artistList = document.createElement('ul');
+
+    // Loop through the artists
+    artists.forEach(artist => {
+      // Create a list item
+      const artistItem = document.createElement('li');
+      // Add the artist name and ID to the list item
+      artistItem.textContent = `${artist.name} (Spotify ID: ${artist.id})`;
+      // Add the list item to the list
+      artistList.appendChild(artistItem);
+    });
+
+    // Add the list to the page
+    document.body.appendChild(artistList);
+
       })
       .catch(error => {
         console.error("Error searching Spotify:", error);
