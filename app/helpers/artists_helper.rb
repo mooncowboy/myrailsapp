7b361d15fd82ef10fd313b69541c278b7bcf2955
Ricardo Fiel <rifiel@microsoft.com>
2025-02-14 11:50:33 +0000

    test

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,2 +1,8 @@
 module ArtistsHelper
+  def star_rating(popularity)
+    stars = (popularity / 20.0).ceil
+    full_stars = '★' * stars
+    empty_stars = '☆' * (5 - stars)
+    full_stars + empty_stars
+  end
 end
