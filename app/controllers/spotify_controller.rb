260e33b8c0f4a2716dc175b3a9d45910bfdd569f
Ricardo Fiel <rifiel@microsoft.com>
2025-03-24 21:53:19 +0000

    added 2 methods for review and comment

--- app/controllers/spotify_controller.rb
+++ app/controllers/spotify_controller.rb
@@ -21,5 +21,53 @@ class SpotifyController < ApplicationController
       redirect_to root_path
     end
 
+    def fetch_user_playlists
+      # Used in Review and Comment only
+      token = session[:spotify_token]
+      url = "https://api.spotify.com/v1/me/playlists"
+      response = HTTParty.get(url, headers: { "Authorization" => "Bearer #{token}" })
+      playlists = JSON.parse(response.body)["items"]
+      result = []
+    
+      for p in playlists do
+        playlist_name = p["name"]
+        track_count = p["tracks"]["total"]
+        result << { title: playlist_name, count: track_count }
+      end
+    
+      if result.length > 0
+        flash[:notice] = "Found #{result.length} playlists"
+      else
+        flash[:alert] = "You have no playlists"
+      end
+    
+      render json: result
+    end
+
+    # def fetch_top_tracks
+    #   # Used in Review and Comment only
+    #   token = session[:spotify_token]
+    #   url = "https://api.spotify.com/v1/me/top/tracks"
+    #   headers = { "Authorization" => "Bearer #{token}" }
+    
+    #   response = HTTParty.get(url, headers: headers)
+    
+    #   if response.success?
+    #     data = JSON.parse(response.body)
+    #     top_tracks = data["items"].map do |track|
+    #       {
+    #         name: track["name"],
+    #         artist: track["artists"].first["name"],
+    #         album: track["album"]["name"],
+    #         popularity: track["popularity"]
+    #       }
+    #     end
+    
+    #     render json: top_tracks
+    #   else
+    #     render json: { error: "Unable to fetch top tracks" }, status: :bad_request
+    #   end
+    # end
+    
   end
   
